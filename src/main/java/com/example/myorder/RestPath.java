6657aacd18849e837c488ad53c471fa45c3cd123
Nicolas Peter Lane <n3ck@tutanota.com>
2020-02-10 21:44:59 -0300

    Criação do controller e service

---
+++ src/main/java/com/example/myorder/RestPath.java
@@ -0,0 +1,5 @@
+package com.example.myorder;
+
+public class RestPath {
+    public static final  String BASE_PATH = "/api/v1";
+}
---
+++ src/main/java/com/example/myorder/api/controllers/RestaurantController.java
@@ -0,0 +1,15 @@
+package com.example.myorder.api.controllers;
+
+import com.example.myorder.RestPath;
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("RestaurantControllerV1")
+@RequestMapping(RestPath.BASE_PATH + "/restaurant")
+@Api(tags = "Restaurantes")
+public class RestaurantController {
+
+
+
+}
