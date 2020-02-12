dd6e7e708b9c3599315f6c0c95561337e7a52c5e
Nicolas Peter Lane <n3ck@tutanota.com>
2020-02-11 21:40:47 -0300

    Criação mapper

--- src/main/java/com/example/myorder/services/UserService.java
+++ src/main/java/com/example/myorder/services/UserService.java
@@ -1,5 +1,9 @@
 package com.example.myorder.services;
 
+import com.example.myorder.api.dtos.CreateUserDto;
+import com.example.myorder.api.dtos.UserResponseDto;
+import com.example.myorder.api.mappers.UserMapper;
+import com.example.myorder.entities.User;
 import com.example.myorder.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +13,9 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public UserResponseDto create(CreateUserDto createUserDto) {
+
+    }
 
 }
